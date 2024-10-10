78ec11cd82e855bedb4f72486d02e3d959bc84c2
Jonathan Brockett <joncbrockett@gmail.com>
2024-10-10 15:14:25 -0700

    added automatic PR for integration of awesome azd

--- scripts/add_awesome_azd/add_azd.py
+++ scripts/add_awesome_azd/add_azd.py
@@ -1,8 +1,11 @@
 import json
 import uuid
+import argparse
 
-def main():
-    with open("./workloads/azd_templates.json", "r") as f:
+def main(root: str, input_file: str, check_quality: bool = False):
+    workloads_file = f"{root}/workloads/workloads.json"
+
+    with open(input_file, "r") as f:
         data = json.load(f)
 
     new_workloads = []
@@ -11,31 +14,30 @@ def main():
     for workload in data:
         if "msft" in workload["tags"]:
             new_workloads.append(workload)
+        elif check_quality:
+            if workload["quality"]:
+                new_workloads.append(workload)
 
     ## Add correct fields
-    with open("./workloads/workloads.json", "r") as f:
+    with open(workloads_file, "r") as f:
         workloads = json.load(f)
     
-    correct_keys = []
+    correct_keys =  workloads[0].keys()
     unique_azd = {}
-    for key in workloads[0].keys():
-        correct_keys.append(key)
-
     for workload in workloads:
         unique_azd[workload["source"]] = workload
 
     ## Add correct keys for new_workloads
-    for workload in new_workloads:
-        if workload["source"] in unique_azd:
-            keys = unique_azd[workload["source"]].keys()
-            for key in keys:
-                if key in workload:
-                    unique_azd[workload["source"]][key] = workload[key]
+    for azd_workload in new_workloads:
+        if azd_workload["source"] in unique_azd:
+            for key in azd_workload.keys():
+                if key in correct_keys:
+                    unique_azd[azd_workload["source"]][key] = azd_workload[key]
         else:
             new_workload = {}
             for key in correct_keys:
-                if key in workload:
-                    new_workload[key] = workload[key]
+                if key in azd_workload:
+                    new_workload[key] = azd_workload[key]
                 else:
                     match key:
                         case "tags":
@@ -52,11 +54,25 @@ def main():
                             new_workload[key] = {}
                         case "id":
                             new_workload[key] = str(uuid.uuid4())
+                        case "tech":
+                            new_workload[key] = []
+                        case "keyFeatures":
+                            new_workload[key] = []
+                        case _:
+                            new_workload[key] = []
             workloads.append(new_workload)
 
     ## Write to file
-    with open("./workloads/new_workloads.json", "w") as f:
+    with open(workloads_file, "w") as f:
         json.dump(workloads, f, indent=4)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Process workloads and add necessary fields.", 
+                                      usage="%(prog)s --root ROOT --input_file INPUT_FILE [--check-quality]")
+    parser.add_argument("-r", "--root", help="Path to the root directory.", required=True)
+    parser.add_argument("-i", "--input_file", help="Path to the input JSON file.", required=True)
+    parser.add_argument("--check-quality", action="store_true", help="Whether to check the quality field in the workloads.")
+
+    args = parser.parse_args()
+
+    main(args.root, args.input_file, args.check_quality)
